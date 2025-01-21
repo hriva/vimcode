766192b6e2b6e0c79a34c7859362639480c5a7e6
hector.riv <books_thief@outlook.com>
2025-01-21 12:18:12 -0600

    ref: basic syntax hl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,35 +11,44 @@ end
 TS_Parsers = {
     'r',
     'python',
+    'cpp',
     'lua',
     'vim',
     'vimdoc',
     'yaml',
     'toml',
     'json',
+    'html',
+    'css',
+    'javascript',
     'regex',
-    'latex',
     'markdown',
     'markdown_inline',
+    'go',
     'sql',
     'bash',
 }
 
 -- highlight
-local disable_highlight = { 'sql', 'latex', 'markdown_inline', 'regex', 'bash' }
+local disable_highlight = { 'sql', 'tex', 'latex', 'markdown_inline', 'regex', 'bash' }
 TS_Parsers_Enabled_for_Highlight = list_filter(TS_Parsers, disable_highlight)
 table.insert(TS_Parsers_Enabled_for_Highlight, 'quarto')
 table.insert(TS_Parsers_Enabled_for_Highlight, 'rmd')
 
 -- indent
-local disable_indent = { 'python', 'sql', 'latex', 'markdown_inline', 'regex', 'bash' }
+local disable_indent = { 'python', 'sql', 'tex', 'latex', 'markdown_inline', 'regex', 'bash' }
 TS_Parsers_Enabled_for_Indent = list_filter(TS_Parsers, disable_indent)
 
 -- fold
 TS_Parsers_Enabled_for_Fold = {
     'python',
     'c',
+    'cpp',
+    'go',
+    'html',
+    'javascript',
     'json',
+    'tex',
     'markdown',
     'lua',
     'query',
@@ -48,7 +57,7 @@ TS_Parsers_Enabled_for_Fold = {
     'yaml',
 }
 
-local disable_textobj = { 'sql', 'latex', 'markdown_inline', 'regex', 'bash', 'vimdoc' }
+local disable_textobj = { 'sql', 'tex', 'latex', 'markdown_inline', 'regex', 'bash', 'vimdoc' }
 TS_Parsers_Enabled_for_Text_Objs = list_filter(TS_Parsers, disable_textobj)
 
 return {
@@ -61,7 +70,7 @@ return {
         cmd = { 'TSUpdate', 'TSInstall' },
         build = ':TSUpdate',
         lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-        branch = 'main',
+        branch = 'master',
         config = function()
             if vim.fn.executable 'tree-sitter' == 0 then
                 if not require('mason-registry').is_installed 'tree-sitter-cli' then
@@ -69,120 +78,29 @@ return {
                 end
             end
 
-            require('nvim-treesitter').setup {
-                ensure_install = TS_Parsers,
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = TS_Parsers,
+                auto_install = false,
+                highlight = {
+                    -- we manage highlight by ourself
+                    enable = false,
+                },
+                indent = {
+                    enable = TS_Parsers_Enabled_for_Indent,
+                },
             }
 
             vim.treesitter.language.register('markdown', { 'quarto', 'rmd' })
 
             autocmd('FileType', {
-                pattern = TS_Parsers_Enabled_for_Indent,
+                pattern = { 'quarto', 'rmd' },
                 group = my_augroup,
-                desc = 'Enable treesitter indent',
+                desc = 'Enable regex highlight with treesitter',
                 callback = function()
-                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                end,
-            })
-
-            autocmd('FileType', {
-                pattern = TS_Parsers_Enabled_for_Fold,
-                group = my_augroup,
-                desc = 'Use treesitter fold',
-                command = 'setlocal foldmethod=expr foldexpr=v:lua.vim.treesitter.foldexpr()',
-            })
-        end,
-    },
-    {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        enabled = false,
-        -- FIXME: must load this plugin earlier otherwise the first file opened
-        -- by vscode cannot activate the treesitter textobjects keymaps.
-        event = { 'VeryLazy' },
-        lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-        branch = 'main',
-        config = function()
-            require('nvim-treesitter-textobjects').setup {
-                select = {
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
-                    include_surrounding_whitespace = false,
-                },
-                move = {
-                    -- whether to set jumps in the jumplist
-                    set_jumps = true,
-                },
-            }
-
-            local selectors = {
-                ['af'] = '@function.outer',
-                ['if'] = '@function.inner',
-                ['ak'] = '@class.outer',
-                ['ik'] = '@class.inner',
-                ['al'] = '@loop.outer',
-                ['il'] = '@loop.inner',
-                ['ac'] = '@conditional.outer',
-                ['ic'] = '@conditional.inner',
-                ['ie'] = '@call.inner',
-                ['ae'] = '@call.outer',
-                ['aA'] = '@parameter.outer', -- mini.ai's regex based argument (`a`) one works better
-                ['iA'] = '@parameter.inner',
-            }
-
-            -- NOTE: The main branch of `nvim-treesitter-textobjects` currently
-            -- contains bug affecting move commands. Await upstream fix to make
-            -- them functional.
-            local movers = {
-                ['f'] = '@function.outer',
-                ['k'] = '@class.outer',
-                ['l'] = '@loop.outer',
-                ['c'] = '@conditional.outer',
-            }
-
-            autocmd('FileType', {
-                pattern = TS_Parsers_Enabled_for_Text_Objs,
-                group = my_augroup,
-                desc = 'Enable treesitter textobjects',
-                callback = function()
-                    for k, obj in pairs(selectors) do
-                        vim.keymap.set({ 'x', 'o' }, k, function()
-                            require('nvim-treesitter-textobjects.select').select_textobject(obj, 'textobjects')
-                        end, {
-                            desc = obj,
-                            buffer = true,
-                        })
-                    end
-
-                    for k, obj in pairs(movers) do
-                        vim.keymap.set({ 'n', 'x', 'o' }, ']' .. k, function()
-                            require('nvim-treesitter-textobjects.move').goto_next_start(obj, 'textobjects')
-                        end, {
-                            desc = obj .. ' next start',
-                            buffer = true,
-                        })
-
-                        vim.keymap.set({ 'n', 'x', 'o' }, '[' .. k, function()
-                            require('nvim-treesitter-textobjects.move').goto_previous_start(obj, 'textobjects')
-                        end, {
-                            desc = obj .. ' prev start',
-                            buffer = true,
-                        })
-
-                        vim.keymap.set({ 'n', 'x', 'o' }, ']' .. k:upper(), function()
-                            require('nvim-treesitter-textobjects.move').goto_next_end(obj, 'textobjects')
-                        end, {
-                            desc = obj .. ' next End',
-                            buffer = true,
-                        })
-
-                        vim.keymap.set({ 'n', 'x', 'o' }, '[' .. k:upper(), function()
-                            require('nvim-treesitter-textobjects.move').goto_previous_end(obj, 'textobjects')
-                        end, {
-                            desc = obj .. ' prev End',
-                            buffer = true,
-                        })
-                    end
+                    vim.cmd [[setlocal syntax=on]]
                 end,
             })
         end,
-    },
+    }
+
 }
